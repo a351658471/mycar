19a1bcadb78ebd158743304d1a606578bb6bd67c
Kzy <351658471@qq.com>
2020-03-24 20:01:25 +0800

    1

--- wx/car-app/miniprogram/pages/addNewCar/addNewCar.js
+++ wx/car-app/miniprogram/pages/addNewCar/addNewCar.js
@@ -1,9 +1,15 @@
 Page({
   data: {
     isOld: true,
-    labelList: ['2.0T-H4', '250P'],
+    typeValue:'',
     isEnter: false,
     showUpload: true,
+    priceValue:0,
+    isNext:false,
+    reqData:{
+      imgList: [],
+      labelList: ['2.0T-H4', '250P'],
+    }
   },
 
   //单选框
@@ -19,6 +25,27 @@ Page({
       })
     }
   },
+  blurEvnet1(e){
+    this.data.priceValue = e.detail.value
+  },
+  blurEvnet2(e) {
+    this.data.typeValue = e.detail.value
+  },
+  blurEvnet3(e) {
+    this.data.reqData.mileageValue = e.detail.value
+  },
+  blurEvnet4(e) {
+    this.data.reqData.firstValue = e.detail.value
+  },
+  blurEvnet5(e) {
+    this.data.reqData.standardValue = e.detail.value
+  },
+  blurEvnet6(e) {
+    this.data.reqData.enginedValue = e.detail.value
+  },
+  blurEvnet7(e) {
+    this.data.reqData.horseValue = e.detail.value
+  },
 
 
   addLabel() {
@@ -29,9 +56,10 @@ Page({
   },
   deleteLabel(e) {
     let index = e.currentTarget.dataset.index
-    this.data.labelList.splice(index, 1)
+    this.data.reqData.labelList.splice(index, 1)
+    let labellist = "reqData.labelList"
     this.setData({
-      labelList: this.data.labelList
+      [labellist]: this.data.reqData.labelList
     })
 
   },
@@ -43,20 +71,72 @@ Page({
   //添加标签确定按钮
   enterEvent(e) {
     console.log("确定")
-    let newArray = this.data.labelList;
+    let newArray = this.data.reqData.labelList;
     newArray.push(e.detail.value);
+    let labellist = "reqData.labelList";
     this.setData({
-      labelList: newArray
+      [labellist]: newArray
+    })
+  },
+  //添加图片
+  addImg() {
+    let count = 3 - this.data.reqData.imgList.length
+    wx.chooseImage({
+      count: count,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        let tempFilePaths = res.tempFilePaths;
+       tempFilePaths.forEach(item=>{
+         this.data.reqData.imgList.push(item)
+       })
+        let imglist = "reqData.imgList"
+        this.setData({
+          [imglist]: this.data.reqData.imgList
+        })
+      }
     })
   },
 
-  uploadImg() {
-    wx.chooseImage({
-      count: 3,
-      success: (res) => {
-        let tempFilePaths = res.tempFilePaths;
-        console.log(res)
+  //删除图片
+  deleteImg(e){
+    let index = e.currentTarget.dataset.index;
+    this.data.reqData.imgList.splice(index, 1);
+    let imglist = "reqData.imgList"
+    this.setData({
+      [imglist]:this.data.imgList
+    })
+  },
+
+  next(){
+    // this.setData({
+    //   isNext:true
+    // })
+    this.data.reqData.isNew = this.data.isOld
+    console.log(this.data.reqData)
+    let data = JSON.stringify(this.data.reqData)
+        // 调用云函数
+    wx.cloud.callFunction({
+      name: 'item',
+      data: {
+        action: "itemAdd",
+        shopid: "f841fd285e71d6900011f3b713c5a83f",
+        item: {
+          name: this.data.typeValue,    // 商品名
+          price: this.data.priceValue,  // 价格
+          stock: 1,       // 库存
+          sort:1002,      // 排序 值越大排越前面
+          data: data,     // 数据
+        }
+      },
+      success: res => {
+        console.log('[云函数] [item.itemAdd] : ', res.result)
+      },
+      fail: err => {
+        console.error('[云函数] [item.itemAdd] 调用失败', err)
       }
     })
   }
+
+
 })
