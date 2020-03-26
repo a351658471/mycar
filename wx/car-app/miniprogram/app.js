53c29554446f064fd34cd5bcb0df0b79eef663d6
fangwy <fangwy2@gmail.com>
2020-03-26 15:27:25 +0800

    首页

--- wx/car-app/miniprogram/app.js
+++ wx/car-app/miniprogram/app.js
@@ -76,6 +76,5 @@ App({
         })
       }
     }
-    this.globalFunc.getShopInfo();
   }
 })
--- wx/car-app/miniprogram/pages/index/index.js
+++ wx/car-app/miniprogram/pages/index/index.js
@@ -1,70 +1,32 @@
+const app = getApp()
+
 Page({
-  data:{
+  data: {
     oldlevel: {
       newCar: 0,
       userdCar: 1
     },
-  imgList:  ['/assets/1.jpg', '/assets/2.jpg', '/assets/3.jpg'],
-    isNew:true,
-    
-  resData:[],
-    newcar:[],
-    oldcar:[]
-  },
+    shop: {},
+    isNew: true,
 
-  //生命周期函数初次渲染完成
-  onLoad: function (){
+    resData: [],
+    newcar: [],
+    oldcar: []
+  },
+  onShow() {
+    app.globalData.addListener(app.globalData.eventShopUpdate, this.onShopInfo)
+    app.globalFunc.getShopInfo();
+  },
+  onHide() {
+    app.globalData.removeListener(app.globalData.eventShopUpdate, this.onShopInfo)
+  },
+  onShopInfo() {
+    this.data.shop = app.globalData.shop
+    this.setData(this.data)
     //获取车列表
     this.getCarData()
-
-        // 调用云函数 获取商店列表
-    wx.cloud.callFunction({
-      name: 'shop',
-      data: {
-        action:"shopList",
-      },
-      success: res => {
-        console.log('[云函数] [shop] : ', res.result)
-      },
-      fail: err => {
-        console.error('[云函数] [shop] 调用失败', err)
-      }
-    });
-
-    //     // 调用云函数  轮播图
-    //  wx.cloud.callFunction({
-    //   name: 'shop',
-    //   data: {
-    //     action: "swiperEdit",
-    //     shopid: "f841fd285e71d6900011f3b713c5a83f",
-    //     swipers: ["1","3"]
-    //   },
-    //   success: res => {
-    //     console.log('[云函数] [shop] : ', res.result)
-    //   },
-    //   fail: err => {
-    //     console.error('[云函数] [shop] 调用失败', err)
-    //   }
-    // });
-   
-
-    //     // 调用云函数  管理员列表
-    // wx.cloud.callFunction({
-    //   name: 'shop',
-    //   data: {
-    //     action:"masterList",
-    //     shopid: "f841fd285e71d6900011f3b713c5a83f",
-    //   },
-    //   success: res => {
-    //     console.log('[云函数] [shop] : ', res.result)
-    //   },
-    //   fail: err => {
-    //     console.error('[云函数] [shop] 调用失败', err)
-    //   }
-    // })
-
   },
-  getCarData(){
+  getCarData() {
     let oldlevel = 0
     if (!this.data.isNew) {
       oldlevel = [this.data.oldlevel.userdCar]
@@ -79,14 +41,14 @@ Page({
         istotal: 0,   //  返回总数
         // 查询条件
         condition: {
-          shopId: "f841fd285e71d6900011f3b713c5a83f",
+          shopId: this.data.shop._id,
           // 名称模糊搜素
           // name: {
           //   $regex: ".*13.*",
           //   $options: 'i'
           // }
         },
-        // status:[2],    // 商品状态 在售 已售 未上架 
+        status:[2],    // 商品状态 0在售 1已售 2未上架 
         oldlevel,
         // 分页
         page: 1,
@@ -95,7 +57,7 @@ Page({
         order: 0
       },
       success: res => {
-        this.data.resData=[]
+        this.data.resData = []
         console.log('[云函数] [item.itemList] : ', res.result)
         res.result.data.forEach(item => {
           if (item.creatime) {
@@ -112,7 +74,7 @@ Page({
       }
     })
   },
-  tabClick: function (e){
+  tabClick: function (e) {
     if (e.detail.tabCurrent == 0) {
       this.setData({
         isNew: true
