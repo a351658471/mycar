a0d945a55ce94f69fedccecb6479449a980f8b40
Kzy <351658471@qq.com>
2020-04-22 16:44:31 +0800

    修复label组件bug

--- wx/car-app/miniprogram/components/label/label.js
+++ wx/car-app/miniprogram/components/label/label.js
@@ -37,37 +37,6 @@ Component({
   },
 
   lifetimes:{
-    // ready(){
-    //   let arr = []
-    //   this.data.newItemData.forEach((item)=>{
-    //     if (this.data.newItemData.length<=2){
-    //        if (item.length > 11) {
-    //          arr.push(item.substring(0, 11))
-    //          this.data.newItemData = arr
-    //        }
-    //        else {
-    //          arr.push(item)
-    //        }
-    //      }
-    //      else{
-    //       arr.push(this.data.newItemData[0].substring(0,7))
-    //       arr.push(this.data.newItemData[1].substring(0, 7))
-    //       arr.push(this.data.newItemData[2].substring(0, 4))
-    //       this.data.newItemData = arr
-    //      }
-    //   })
-    //   if (this.data.isDetail){
-    //     arr.splice(3)
-    //     this.setData({
-    //       newItemData: arr
-    //     })
-    //   }
-    //   else{
-    //     this.setData({
-    //       newItemData: arr
-    //     })
-    //   }
-    // }
   },
   /**
    * 组件的方法列表
